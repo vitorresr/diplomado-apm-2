0deabb5f7906fadbd1c31d22bf76de2e5e5276bd
vitorresr <vpaolillatr@gmail.com>
2015-05-09 12:30:52 -0500

    cordova
    lab cordova

---
+++ lab36/platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,37 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device-motion/www/Acceleration.js",
+        "id": "cordova-plugin-device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-motion/www/accelerometer.js",
+        "id": "cordova-plugin-device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-motion/src/browser/AccelerometerProxy.js",
+        "id": "cordova-plugin-device-motion.AccelerometerProxy",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device-motion/src/browser/accelerometer.js",
+        "id": "cordova-plugin-device-motion.accelerometer",
+        "merges": [
+            "navigator.accelerometer"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-device-motion": "1.0.0"
+}
+// BOTTOM OF METADATA
+});

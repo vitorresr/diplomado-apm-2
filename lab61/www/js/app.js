00ebc7b75efe7e728d1ba710b946b833df6ecd7c
vitorresr <vpaolillatr@gmail.com>
2015-06-06 12:06:34 -0500

    laboratorios ionic
    laboratorios ionic

---
+++ lab61/www/js/app.js
@@ -0,0 +1,27 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+ .controller('pruebaController', ['$scope', function($scope){
+                 $scope.value_uno = 90;
+                  $scope.min_uno = 85;
+                   $scope.max_uno = 92;
+             }]);
+
+
